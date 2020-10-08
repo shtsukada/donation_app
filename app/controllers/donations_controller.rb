fe16e0eba9d16d520ce20f55d0c6c43a2b172886
shtsukada <st01251988@gmail.com>
2020-10-08 17:06:22 +0900

    first commit

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,26 @@
+class  DonationsController < ApplicationController
+  def index
+  end
+ 
+  def new
+    @donation = UserDonation.new   #「UserDonation」に編集
+  end
+  
+  def create
+    @donation = UserDonation.new(donation_params)
+ 
+    if @donation.valid?
+      @donation.save  # バリデーションをクリアした時
+      return redirect_to root_path
+    else
+      render "new"    # バリデーションに弾かれた時
+    end
+  end
+
+  private
+
+  def donation_params
+    params.require(:user_donation).permit(:name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :price)
+  end
+
+end
